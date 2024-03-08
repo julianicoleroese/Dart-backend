3a7ad83585f8c58ac09ba4653254bec7db66b71f
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-03-08 11:51:34 -0300

    atv.dart

---
+++ atv.dart
@@ -0,0 +1,30 @@
+//Crie um formulário seu, diga o seu nome, seu generoFavorito de jogo, seu jogoFavorito, e se joga no pc ou console.
+
+void main() {
+  String nome = 'Julia';
+  String generoFavorito = 'ação-aventura';
+  String jogoFavorito = 'Dead by Daylight';
+  String jogoFavorito2 = 'Red Dead Redemption 2';
+  String pc = 'PC';
+
+  print('Meu nome é ${nome}, jogo no ${pc}, tenhos dois jogos favoritos que são ${jogoFavorito} e ${jogoFavorito2} e o genero de jogo que mais gosto é ${generoFavorito}.');
+
+  
+//Média
+  int nota1 = 90;
+  int nota2 = 80;
+  double media = ((nota1 + nota2) / 2);
+  print('A média é ${media.toStringAsFixed(2)}.');
+  
+  
+//Faça um algoritmo que calcule o IMC de uma pessoa, a altura e o resultado tem que ser em double, calculo = (peso / (altura * altura)) , retorne = 'a ${pessoa} tem o IMC de ${imc.toStringAsFixed(2)}'
+  
+  String nome2 = 'Julia';
+  int peso = 61;
+  double altura = 1.69;
+  double imc = peso / (altura * altura);
+  print('O ${nome2} possue um IMC de ${imc.toStringAsFixed(2)');
+  
+ 
+  
+}
