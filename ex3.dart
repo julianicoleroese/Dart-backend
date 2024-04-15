579029b45870edc695932a105b25be76d45c3af6
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-04-15 11:19:19 -0300

    ex3.dart

---
+++ ex3.dart
@@ -0,0 +1,10 @@
+void main() {
+  print(division(89,54));
+  print(division(65,46));
+  print(division(11,76));
+  print(division(54,23));
+}
+
+double division(double num1, double num2) {
+  return num1 / num2;
+}
