8781792285bfc642608cd58465c3964a6ba10c10
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-04-22 11:31:37 -0300

    ex12.dart

---
+++ ex12.dart
@@ -0,0 +1,11 @@
+void main() {
+   print(sum(543, 4234))
+}
+
+double sum(double num1, double num2) =>num1 + num2;
+double sub(double num1, double2) =>num1 - num2;
+double mult(double num1, double num2) =>num1 * num2;
+double div(double num1, double2) =>num1 / num2;
+  
+
+
